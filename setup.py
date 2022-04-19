d2f1fd1e8e553fba170d8c760dc79b026465aedb
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2022-04-19 17:04:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ extras = {
     "test": ["pytest>=3.4", "jsonschema", "pypatchelf", "pretend", "docker"],
     "coverage": ["pytest-cov"],
 }
-extras["develop"] = sum(extras.values(), [])
 extras["coverage"] += extras["test"]
 
 setup(extras_require=extras)
